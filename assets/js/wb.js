68b3761eec8a5cd5868a1fb922070d5d5b4313fb
ysEnmanuel <cristopherariasp@icloud.com>
2025-03-13 11:01:21 -0500

    Update WhatsApp contact number and improve service descriptions in HTML

--- assets/js/wb.js
+++ assets/js/wb.js
@@ -1,10 +1,11 @@
-$(function() {
-  $('#WAButton').floatingWhatsApp({
-    phone: '51999934443',
-    headerTitle: 'Chatea Por Whatsapp!',
-    popupMessage: 'Hola,como lo puedo ayudar?',
-    showPopup: true,
-    buttonImage: '<img alt="Boton Whatsapp" src="assets/img/whatsapp.svg"/>',
-    position: "left"    
-  });
+$(function () {
+    $("#WAButton").floatingWhatsApp({
+        phone: "51017548441",
+        headerTitle: "Chatea Por Whatsapp!",
+        popupMessage: "Hola,como lo puedo ayudar?",
+        showPopup: true,
+        buttonImage:
+            '<img alt="Boton Whatsapp" src="assets/img/whatsapp.svg"/>',
+        position: "left",
+    });
 });
